d154cf39c558daa9340c1e37c1079c59e3100aaf
Guy <guy,redid@gmail.com>
2022-03-01 15:06:28 +0200

    something is working :)

--- 4 - upload new file to system/test wxpython/test1.py
+++ 4 - upload new file to system/test wxpython/test1.py
@@ -5,7 +5,7 @@ import string
 
 class MyFrame(wx.Frame):
     def __init__(self, parent=None):
-        super(MyFrame, self).__init__(parent, title="GTorrent", size=(500, 500))
+        super(MyFrame, self).__init__(parent, title="GTorrent", size=wx.DisplaySize())
         # create status bar
         # self.CreateStatusBar(1)
         # self.SetStatusText("Developed by Merry Geva 1/1/2000")
@@ -43,7 +43,8 @@ class MainPanel(wx.Panel):
 
 class FilesPanel(wx.Panel):
     def __init__(self, parent, frame):
-        wx.Panel.__init__(self, parent, pos=wx.DefaultPosition, size=(500, 500), style=wx.SIMPLE_BORDER)
+        # wx.Panel.__init__(self, parent, pos=wx.DefaultPosition, size=(500, 500), style=wx.SIMPLE_BORDER)
+        wx.Panel.__init__(self, parent, pos=wx.DefaultPosition, size=wx.DisplaySize(), style=wx.SIMPLE_BORDER)
         self.frame = frame
         self.parent = parent
         self.file_select = None
@@ -51,51 +52,88 @@ class FilesPanel(wx.Panel):
         self.Hide()
         self.SetBackgroundColour(wx.LIGHT_GREY)
 
+        titlefont = wx.Font(22, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+
         sizer = wx.BoxSizer(wx.VERTICAL)
 
-        # title
-        title = wx.StaticText(self, -1, label="Files Panel")
-        titlefont = wx.Font(22, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
-        title.SetForegroundColour(wx.BLACK)
-        title.SetFont(titlefont)
+        logoImg = wx.Image("Full logo.png", wx.BITMAP_TYPE_ANY).Rescale(600, 300)
+        logoImg = wx.StaticBitmap(self, -1, wx.BitmapFromImage(logoImg))
+
+        # sizer.AddSpacer(10)
+        sizer.Add(logoImg, 0, wx.ALIGN_CENTER | wx.ALL, 0)
+
+        text = wx.StaticText(self, -1, label="Files in the system: ")
+        text.SetFont(titlefont)
+        sizer.AddSpacer(10)
+        sizer.Add(text, 0, wx.ALIGN_CENTER | wx.ALL, 0)
 
         # scrolled panel
-        self.scrollP = scrolled.ScrolledPanel(self, -1, style=wx.TAB_TRAVERSAL | wx.SUNKEN_BORDER, size=(500, 300))
+        self.scrollP = scrolled.ScrolledPanel(self, -1, style=wx.TAB_TRAVERSAL | wx.SUNKEN_BORDER, size=(600, 300))
+        # self.scrollP.Centre()
         self.scrollP.SetAutoLayout(1)
         self.scrollP.SetupScrolling()
 
         self.spSizer = wx.BoxSizer(wx.VERTICAL)
 
-        fileImg = wx.Bitmap("file.png")
-        # wx.StaticBitmap(self, -1, fileImg, (10, 5), (fileImg.getWidth(), fileImg.getHeight()))
-        # create button at point (20, 20)
-        self.st = wx.Button(self, id=1, label="Button", pos=(100, 100),
-                            size=(100, 30), name="button")
-        self.st.SetBitmap(fileImg)
+        files_list = ["pug.jpg", 'cat.jpg', "file1.png", "test", "a", "b", "c", "d"]
 
-        files_list = ["pug.jpg", 'cat.jpg', "file1.png"]
+        # sub_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        # for word in words:
+        # add files to the available files' list
         for file in files_list:
-            pass
-            # sub_sizer = wx.BoxSizer(wx.HORIZONTAL)
-            # sub_sizer.Add(fileImg)
-            # text = wx.TextCtrl(self.scrollP, value=file)
-            # text.Bind(wx.EVT_BUTTON, self.file_selected)
-            # self.spSizer.Add(text)
+            sub_sizer = wx.BoxSizer(wx.HORIZONTAL)
+            text = wx.StaticText(self.scrollP, -1, label=file)
+            text.SetFont(titlefont)
+
+            fileImg = wx.Image("file.png", wx.BITMAP_TYPE_ANY).Rescale(75, 75)
+            fileImg = wx.StaticBitmap(self.scrollP, -1, wx.BitmapFromImage(fileImg))
+
+            # downloadImg = wx.Image("download.jpg", wx.BITMAP_TYPE_ANY).Rescale(75, 75)
+            # downloadImg = wx.StaticBitmap(self.scrollP, -1, wx.BitmapFromImage(downloadImg))
+            #
+            # upBtn = wx.BitmapButton(self.scrollP, name="download file", size=(75, 75), bitmap=downloadImg)
+            # upBtn.Bind(wx.EVT_BUTTON, self.file_selected)
+            # upBtn.SetToolTip("download file")
+            # upBtn.SetBackgroundColour('light grey')
+
+            # bottom_buttons_sizer.Add(upBtn)
+
+            fileBtn = wx.Button(self.scrollP, id=1, label="Download", size=(100, 30), name=file)
+            fileBtn.Bind(wx.EVT_BUTTON, self.file_selected)
+
+            sub_sizer.Add(fileImg, 0, wx.ALL, 0)
+            sub_sizer.AddSpacer(15)
+            sub_sizer.Add(text, 0, wx.ALL, 0)
+            sub_sizer.AddSpacer(30)
+            sub_sizer.Add(fileBtn, 0, wx.ALL, 0)
+
+            self.spSizer.Add(sub_sizer)
+
         self.scrollP.SetSizer(self.spSizer)
+        self.scrollP.SetupScrolling()
+
+        sizer.Add(self.scrollP, 0, wx.ALIGN_CENTER | wx.ALL, 0)
+
+        sizer.AddSpacer(10)
+
+        bottom_buttons_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         # load image button
         upImage = wx.Image("upload.png")
         upImage.Rescale(50, 50)
         upImage = wx.Bitmap(upImage)
-        upBtn = wx.BitmapButton(self, name="upload file", size=(50, 50), bitmap=upImage, pos=(200, 200))
+        upBtn = wx.BitmapButton(self, name="upload file", size=(50, 50), bitmap=upImage, pos=(200, 800))
         upBtn.Bind(wx.EVT_BUTTON, self.uploadImage)
         upBtn.SetToolTip("upload file")
         upBtn.SetBackgroundColour('light grey')
 
+        bottom_buttons_sizer.Add(upBtn)
+        sizer.Add(bottom_buttons_sizer, 0, wx.ALIGN_CENTER | wx.ALL, 0)
+
+        self.SetSizer(sizer)
+
     def file_selected(self, event):
-        print(event.GetWindow().GetValue())
+        print(event.GetEventObject().GetName())
 
     def uploadImage(self, event):
         print("in Upload")
@@ -122,10 +160,6 @@ class FilesPanel(wx.Panel):
 
         else:
             wx.MessageBox("You must select file before")
-
-    def file_selected(self, event):
-        self.file_select = event.GetWindow().GetValue()
-        self.element = event.GetWindow()
 
 
 app = wx.App()
