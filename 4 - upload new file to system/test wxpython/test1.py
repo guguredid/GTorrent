76440a5f819847881e87783a8b8c646ce8778bd8
Guy <guy,redid@gmail.com>
2022-03-03 11:42:27 +0200

    First version of graphics - works!

--- 4 - upload new file to system/test wxpython/test1.py
+++ 4 - upload new file to system/test wxpython/test1.py
@@ -52,7 +52,7 @@ class FilesPanel(wx.Panel):
         self.SetBackgroundColour(wx.LIGHT_GREY)
 
         # text font
-        titlefont = wx.Font(22, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        self.titlefont = wx.Font(22, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
 
         # main sizer
         sizer = wx.BoxSizer(wx.VERTICAL)
@@ -61,10 +61,9 @@ class FilesPanel(wx.Panel):
         logoImg = wx.Image("Full logo.png", wx.BITMAP_TYPE_ANY).Rescale(600, 300)
         logoImg = wx.StaticBitmap(self, -1, wx.BitmapFromImage(logoImg))
 
-        # sizer.AddSpacer(10)
         # text
         text = wx.StaticText(self, -1, label="Files in the system: ")
-        text.SetFont(titlefont)
+        text.SetFont(self.titlefont)
         sizer.AddSpacer(10)
 
         sizer.Add(logoImg, 0, wx.ALIGN_CENTER | wx.ALL, 0)
@@ -72,43 +71,18 @@ class FilesPanel(wx.Panel):
 
         # scrolled panel
         self.scrollP = scrolled.ScrolledPanel(self, -1, style=wx.TAB_TRAVERSAL | wx.SUNKEN_BORDER, size=(600, 300))
-        # self.scrollP.Centre()
         self.scrollP.SetAutoLayout(1)
         self.scrollP.SetupScrolling()
 
         # the sizer for the scrolled panel
         self.spSizer = wx.BoxSizer(wx.VERTICAL)
 
-        files_list = ["pug.jpg", 'cat.jpg', "file1.png", "test", "a", "b", "c", "d"]
+        files_list = ["pug.jpg", 'cat.jpg', "file1.png", "test", "a", "b", "c", "d", "ttttttttttttttttt"]
 
         # add files to the available files' list
         for file in files_list:
-            # sub sizer for each file
-            sub_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
-            # the file name
-            text = wx.StaticText(self.scrollP, -1, label=file)
-            text.SetFont(titlefont)
-
-            # the file icon
-            fileImg = wx.Image("file.png", wx.BITMAP_TYPE_ANY).Rescale(75, 75)
-            fileImg = wx.StaticBitmap(self.scrollP, -1, wx.BitmapFromImage(fileImg))
-
-            # the download button
-            downloadImg = wx.Image("download.png", wx.BITMAP_TYPE_ANY).Rescale(75, 75)
-            downloadImg = wx.Bitmap(downloadImg)
-            downBtn = wx.BitmapButton(self.scrollP, name=file, size=(75, 75), bitmap=downloadImg)
-            downBtn.Bind(wx.EVT_BUTTON, self.file_selected)
-
-            # add everything
-            sub_sizer.Add(fileImg, 0, wx.ALL, 0)
-            sub_sizer.AddSpacer(15)
-            sub_sizer.Add(text, 0, wx.ALL, 0)
-            sub_sizer.AddSpacer(30)
-            sub_sizer.Add(downBtn, 0, wx.ALL, 0)
-
-            # add the file's sizer to the scrolled panel's sizer
-            self.spSizer.Add(sub_sizer)
+            self.add_file(file)
 
         # add the scrolled panel's sizer to the main sizer
         self.scrollP.SetSizer(self.spSizer)
@@ -125,13 +99,12 @@ class FilesPanel(wx.Panel):
         upImage = wx.Image("upload.png")
         upImage.Rescale(50, 50)
         upImage = wx.Bitmap(upImage)
-        upBtn = wx.BitmapButton(self, name="upload file", size=(50, 50), bitmap=upImage, pos=(200, 800))
+        upBtn = wx.BitmapButton(self, name="upload file", size=(75, 75), bitmap=upImage, pos=(200, 800))
         upBtn.Bind(wx.EVT_BUTTON, self.uploadImage)
         # upBtn.SetToolTip("upload file")
-        # upBtn.SetBackgroundColour('light grey')
 
         # change GTorrent's directory button
-        changeDirBtn = wx.Button(self, id=1, label="Change Directory", size=(200, 100), name="changeDir")
+        changeDirBtn = wx.Button(self, id=1, label="Change Directory", size=(100, 75), name="changeDir")
         changeDirBtn.Bind(wx.EVT_BUTTON, self.updateDir)
 
         # update the sizers
@@ -142,6 +115,50 @@ class FilesPanel(wx.Panel):
         sizer.Add(bottom_buttons_sizer, 0, wx.ALIGN_CENTER | wx.ALL, 0)
 
         self.SetSizer(sizer)
+
+    def add_file(self, filename):
+        '''
+        adds file name to the scrolled bar
+        '''
+        # sub sizer for each file
+        sub_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        # the file name
+        text = wx.StaticText(self.scrollP, -1, label=filename)
+        text.SetFont(self.titlefont)
+
+        # the file icon
+        fileImg = wx.Image("file.png", wx.BITMAP_TYPE_ANY).Rescale(75, 75)
+        fileImg = wx.StaticBitmap(self.scrollP, -1, wx.BitmapFromImage(fileImg))
+
+        # the download button
+        downloadImg = wx.Image("download2.png", wx.BITMAP_TYPE_ANY).Rescale(75, 75)
+
+        # downloadImg = wx.Image("download.jpg", wx.BITMAP_TYPE_ANY).Rescale(75, 75)
+        downloadImg = wx.Bitmap(downloadImg)
+        downBtn = wx.BitmapButton(self.scrollP, name=filename, size=(75, 75), bitmap=downloadImg)
+        downBtn.Bind(wx.EVT_BUTTON, self.file_selected)
+
+        downBtn.SetBackgroundColour(wx.LIGHT_GREY)
+        downBtn.SetWindowStyleFlag(wx.NO_BORDER)
+
+        spaceFromLeft = downloadImg.GetSize()[0] / 2 + downloadImg.GetSize()[0]
+        spaceFromRight = fileImg.GetSize()[0] + text.GetSize()[0]
+
+        # add everything
+        sub_sizer.Add(fileImg, 0, wx.ALL, 0)
+        sub_sizer.AddSpacer(15)
+        sub_sizer.Add(text, 0, wx.ALL, 0)
+        sub_sizer.AddSpacer(600 - (spaceFromLeft + spaceFromRight))
+        sub_sizer.Add(downBtn, 0, wx.ALL, 0)
+
+        # add the file's sizer to the scrolled panel's sizer
+        self.spSizer.Add(sub_sizer)
+
+        self.scrollP.SetSizer(self.spSizer)
+        self.scrollP.SetupScrolling()
+        self.scrollP.Layout()
+
 
     def file_selected(self, event):
         '''
@@ -161,6 +178,7 @@ class FilesPanel(wx.Panel):
         if path:
             fileName = path[path.rfind("\\")+1:]
             print(f"PATH-{path};;;;;NAME-{fileName}")
+            self.add_file(fileName)
 
     def updateDir(self, event):
         '''
